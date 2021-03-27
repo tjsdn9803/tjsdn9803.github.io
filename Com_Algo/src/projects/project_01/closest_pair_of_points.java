8c4c1602ba96695526a86a3289db4e06102b725d
tjsdn9803 <leeinjoo2@inu.ac.kr>
2021-03-27 19:59:48 +0900

    Add [branch test]

--- Com_Algo/src/projects/project_01/closest_pair_of_points.java
+++ Com_Algo/src/projects/project_01/closest_pair_of_points.java
@@ -3,5 +3,6 @@ package projects.project_01;
 public class closest_pair_of_points {
     public static void main(String[] args) {
         System.out.println("closest pair of points");
+	System.out.println("branch test");
     }
 }
